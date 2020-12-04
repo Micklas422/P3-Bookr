27e90a8b43404b164f86cb0c6c07da5edbb27390
Micklas Skov Skelmose <kontakt@opetum.dk>
2020-12-04 13:17:43 +0100

    Fixed what Lars could not

---
+++ P3-Bookr/Frontpage/FrontPage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P3_Bookr.Windows
+{
+    public partial class FrontPage : Form
+    {
+        public FrontPage()
+        {
+        }
+
+        private void FrontPage_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
