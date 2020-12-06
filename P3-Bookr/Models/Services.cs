11d0a8b338c643e0864385f0e296be5e3aa71042
Nightbue <khpe19@student.aau.dk>
2020-12-06 20:29:33 +0100

    Update Services.cs

--- P3-Bookr/Models/Services.cs
+++ P3-Bookr/Models/Services.cs
@@ -8,5 +8,31 @@ namespace P3_Bookr.Models
 {
     class Services
     {
+        string _name;
+        bool _departmentDeactivatet;
+        public enum ServiceState
+        {
+            //implemeteres i commons/membertypes
+        };
+        public enum ServiceType
+        {
+            //implemeteres i commons/membertypes
+        };
+        public Services(string name, bool departmentDeactivatet)
+        {
+            _name = name;
+            _departmentDeactivatet = departmentDeactivatet;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public bool DepartmentDeactivatet
+        {
+            get { return _departmentDeactivatet; }
+            set { _departmentDeactivatet = value; }
+        }
     }
 }
