d3a55d2df1bf7828e593262d63bbf75a218ed7fb
Nightbue <khpe19@student.aau.dk>
2020-12-06 19:34:31 +0100

    ServiceInfoPanel

---
+++ P3-Bookr/Windows/ServiceOptionCalendarPanel.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace P3_Bookr.Windows
+{
+    partial class ServiceOptionCalendarPanel
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ServiceOptionCalendarPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "ServiceOptionCalendarPanel";
+            this.Size = new System.Drawing.Size(240, 300);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
