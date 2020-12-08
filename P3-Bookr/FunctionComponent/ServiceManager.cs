bc999c6abf620af739b5eb7eb44fbc022d952dde
creative-break <58289859+creative-break@users.noreply.github.com>
2020-12-08 10:46:38 +0100

    Update to FunctionComponent innterface

---
+++ P3-Bookr/FunctionComponent/ServiceManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3_Bookr.FunctionComponent
+{
+    class ServiceManager : IServiceManager
+    {
+        public void Reserve()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SeeAvailability()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
