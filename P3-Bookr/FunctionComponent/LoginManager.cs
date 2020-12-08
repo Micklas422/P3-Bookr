7c98bbcfb786565cf0290fc5f0be1e29e24d612c
Rune <58289859+creative-break@users.noreply.github.com>
2020-12-08 23:04:33 +0100

    ValidateLogin

--- P3-Bookr/FunctionComponent/LoginManager.cs
+++ P3-Bookr/FunctionComponent/LoginManager.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using P3_Bookr.Models;
 
 namespace P3_Bookr.FunctionComponent
 {
     public class LoginManager : ILoginManager
     {
-        
+        readonly Member member;
         public bool ValidateLogin(string username, string password)
         {
-
             
-            return true;
+            if (member.Username == username && member.Password == password)
+            {
+                return true;
+            }
+            else 
+                return false;
         }
     }
 }
