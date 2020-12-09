0fe0f9ebe8491a2c248724a6b7faf6fc47d1ea0c
Nightbue <khpe19@student.aau.dk>
2020-12-09 10:56:22 +0100

    DAL interface

---
+++ P3-Bookr/DAL/DataAccesLayer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using P3_Bookr.DAL.Interfaces;
+
+namespace P3_Bookr.DAL
+{
+    class DataAccesLayer : IDataAccesLayer
+    {
+        ICustomerDAL _customerDAL;
+        IDepartmentDAL _departmentDAL;
+        IMemberDAL _memberDAL;
+        IPaymentDAL _paymentDAL;
+        IReservationDAL _reservationDAL;
+        IServiceGroupDAL _serviceGroupDAL;
+        IServiceOfferingDAL _serviceOfferingDAL;
+        IServicesDAL _servicesDAL;
+        ITimeperiodDAL _timeperiodDAL;
+
+        public DataAccesLayer()
+        {
+            _customerDAL = new CustomerDAL();
+            _departmentDAL = new DepartmentDAL();
+            _memberDAL = new MemberDAL();
+            _paymentDAL = new PaymentDAL();
+            _reservationDAL = new ReservationDAL();
+            _serviceGroupDAL = new ServiceGroupDAL();
+            _serviceOfferingDAL = new ServiceOfferingDAL();
+            //_servicesDAL = new ServicesDAL();
+        }
+
+        ICustomerDAL CustomerDAL
+        {
+            get { return _customerDAL; }
+        }
+    }
+}
---
+++ P3-Bookr/DAL/Interfaces/IDataAccesLayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3_Bookr.DAL.Interfaces
+{
+    interface IDataAccesLayer
+    {
+        ICustomerDAL customerDAL { get; }
+        IDepartmentDAL departmentDAL { get; }
+        IMemberDAL memberDAL { get; }
+        IPaymentDAL paymentDAL { get; }
+        IReservationDAL reservationDAL { get; }
+        IServiceGroupDAL serviceGroupDAL { get; }
+        IServiceOfferingDAL serviceOfferingDAL { get; }
+        IServicesDAL servicesDAL { get; }
+        ITimeperiodDAL timeperiodDAL { get; }
+    }
+}
