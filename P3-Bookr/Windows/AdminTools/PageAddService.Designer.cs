486796e264060db84bace6d1f98c5a5752b60f4e
Heresy <claesm1999@gmail.com>
2020-12-13 23:10:39 +0100

    Added admin tool page: PageAddService as a usercontrol

---
+++ P3-Bookr/Windows/AdminTools/PageAddService.Designer.cs
@@ -0,0 +1,47 @@
+﻿
+namespace P3_Bookr.Windows.AdminTools
+{
+    partial class PageAddService
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // PageAddService
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(225)))), ((int)(((byte)(227)))));
+            this.Name = "PageAddService";
+            this.Size = new System.Drawing.Size(1315, 880);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
