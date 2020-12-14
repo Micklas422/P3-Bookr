3e33dfe3a956182631d3000faf1922516677d597
Frederik <frederik@zenziot.dk>
2020-12-14 14:15:20 +0100

    fixed names

--- P3-Bookr/Windows/ServicePanels/ServicesTop.cs
+++ P3-Bookr/Windows/ServicePanels/ServicesTop.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 using P3_Bookr.Models;
 using P3_Bookr.Commons.Enums;
 
-namespace P3_Bookr.Windows.Services
+namespace P3_Bookr.Windows
 {
     public partial class ServicesTop : UserControl
     {
