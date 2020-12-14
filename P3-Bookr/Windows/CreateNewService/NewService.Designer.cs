1d81cb60a9245d18a4ea1ae1c597fbdef188442a
Heresy <claesm1999@gmail.com>
2020-12-14 12:04:56 +0100

    Changed where service is created from UserControl to NewService page

--- P3-Bookr/Windows/CreateNewService/NewService.Designer.cs
+++ P3-Bookr/Windows/CreateNewService/NewService.Designer.cs
@@ -48,9 +48,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(16, 16);
+            this.label1.Location = new System.Drawing.Point(21, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(159, 31);
+            this.label1.Size = new System.Drawing.Size(199, 39);
             this.label1.TabIndex = 0;
             this.label1.Text = "Oplysninger";
             // 
@@ -58,68 +59,76 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(22, 68);
+            this.label2.Location = new System.Drawing.Point(29, 84);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(59, 24);
+            this.label2.Size = new System.Drawing.Size(74, 29);
             this.label2.TabIndex = 1;
             this.label2.Text = "Navn:";
             // 
             // textBoxName
             // 
-            this.textBoxName.Location = new System.Drawing.Point(87, 73);
+            this.textBoxName.Location = new System.Drawing.Point(116, 90);
+            this.textBoxName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxName.Name = "textBoxName";
-            this.textBoxName.Size = new System.Drawing.Size(300, 20);
+            this.textBoxName.Size = new System.Drawing.Size(399, 22);
             this.textBoxName.TabIndex = 2;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(426, 68);
+            this.label3.Location = new System.Drawing.Point(568, 84);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(85, 24);
+            this.label3.Size = new System.Drawing.Size(108, 29);
             this.label3.TabIndex = 3;
             this.label3.Text = "Adresse:";
             // 
             // textBoxAddress
             // 
-            this.textBoxAddress.Location = new System.Drawing.Point(542, 73);
+            this.textBoxAddress.Location = new System.Drawing.Point(723, 90);
+            this.textBoxAddress.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxAddress.Name = "textBoxAddress";
-            this.textBoxAddress.Size = new System.Drawing.Size(200, 20);
+            this.textBoxAddress.Size = new System.Drawing.Size(265, 22);
             this.textBoxAddress.TabIndex = 4;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(22, 118);
+            this.label4.Location = new System.Drawing.Point(29, 145);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(58, 24);
+            this.label4.Size = new System.Drawing.Size(74, 29);
             this.label4.TabIndex = 6;
             this.label4.Text = "Type:";
             // 
             // textBoxTypr
             // 
-            this.textBoxTypr.Location = new System.Drawing.Point(86, 123);
+            this.textBoxTypr.Location = new System.Drawing.Point(115, 151);
+            this.textBoxTypr.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxTypr.Name = "textBoxTypr";
-            this.textBoxTypr.Size = new System.Drawing.Size(300, 20);
+            this.textBoxTypr.Size = new System.Drawing.Size(399, 22);
             this.textBoxTypr.TabIndex = 7;
             // 
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.BackColor = System.Drawing.Color.White;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(26, 214);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(35, 263);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(360, 210);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(480, 258);
             this.flowLayoutPanel1.TabIndex = 9;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(23, 167);
+            this.label5.Location = new System.Drawing.Point(31, 206);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(46, 24);
+            this.label5.Size = new System.Drawing.Size(61, 29);
             this.label5.TabIndex = 10;
             this.label5.Text = "Pris:";
             // 
@@ -128,9 +137,10 @@
             this.ButtonNewService.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(172)))), ((int)(((byte)(44)))));
             this.ButtonNewService.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ButtonNewService.ForeColor = System.Drawing.Color.White;
-            this.ButtonNewService.Location = new System.Drawing.Point(86, 162);
+            this.ButtonNewService.Location = new System.Drawing.Point(115, 199);
+            this.ButtonNewService.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ButtonNewService.Name = "ButtonNewService";
-            this.ButtonNewService.Size = new System.Drawing.Size(300, 38);
+            this.ButtonNewService.Size = new System.Drawing.Size(400, 47);
             this.ButtonNewService.TabIndex = 11;
             this.ButtonNewService.Text = "+ Nyt Service Tilbud";
             this.ButtonNewService.UseVisualStyleBackColor = false;
@@ -139,17 +149,19 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(426, 119);
+            this.label6.Location = new System.Drawing.Point(568, 146);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(110, 24);
+            this.label6.Size = new System.Drawing.Size(144, 29);
             this.label6.TabIndex = 12;
             this.label6.Text = "Beskrivelse:";
             // 
             // richTextBoxDescription
             // 
-            this.richTextBoxDescription.Location = new System.Drawing.Point(542, 123);
+            this.richTextBoxDescription.Location = new System.Drawing.Point(723, 151);
+            this.richTextBoxDescription.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.richTextBoxDescription.Name = "richTextBoxDescription";
-            this.richTextBoxDescription.Size = new System.Drawing.Size(200, 180);
+            this.richTextBoxDescription.Size = new System.Drawing.Size(265, 221);
             this.richTextBoxDescription.TabIndex = 14;
             this.richTextBoxDescription.Text = "";
             // 
@@ -158,28 +170,31 @@
             this.ButtonCreate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(95)))), ((int)(((byte)(88)))));
             this.ButtonCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ButtonCreate.ForeColor = System.Drawing.Color.White;
-            this.ButtonCreate.Location = new System.Drawing.Point(488, 384);
+            this.ButtonCreate.Location = new System.Drawing.Point(651, 473);
+            this.ButtonCreate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ButtonCreate.Name = "ButtonCreate";
-            this.ButtonCreate.Size = new System.Drawing.Size(140, 40);
+            this.ButtonCreate.Size = new System.Drawing.Size(187, 49);
             this.ButtonCreate.TabIndex = 18;
             this.ButtonCreate.Text = "Opret";
             this.ButtonCreate.UseVisualStyleBackColor = false;
+            this.ButtonCreate.Click += new System.EventHandler(this.ButtonCreate_Click);
             // 
             // ButtonCancel
             // 
             this.ButtonCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(128)))), ((int)(((byte)(116)))));
             this.ButtonCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ButtonCancel.ForeColor = System.Drawing.Color.White;
-            this.ButtonCancel.Location = new System.Drawing.Point(662, 384);
+            this.ButtonCancel.Location = new System.Drawing.Point(883, 473);
+            this.ButtonCancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ButtonCancel.Name = "ButtonCancel";
-            this.ButtonCancel.Size = new System.Drawing.Size(140, 40);
+            this.ButtonCancel.Size = new System.Drawing.Size(187, 49);
             this.ButtonCancel.TabIndex = 19;
             this.ButtonCancel.Text = "Annuller";
             this.ButtonCancel.UseVisualStyleBackColor = false;
             // 
             // NewService
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(225)))), ((int)(((byte)(227)))));
             this.Controls.Add(this.ButtonCancel);
@@ -196,8 +211,9 @@
             this.Controls.Add(this.textBoxName);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "NewService";
-            this.Size = new System.Drawing.Size(829, 474);
+            this.Size = new System.Drawing.Size(1105, 583);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- P3-Bookr/Windows/CreateNewService/NewService.cs
+++ P3-Bookr/Windows/CreateNewService/NewService.cs
@@ -7,14 +7,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using P3_Bookr.Windows.WindowsInterfaces;
+using P3_Bookr.Models;
+
 
 namespace P3_Bookr.Windows.CreateNewService
 {
     public partial class NewService : UserControl
     {
+        IAdminToolsUI _handler;
         public NewService()
         {
             InitializeComponent();
+        }
+
+        private void ButtonCreate_Click(object sender, EventArgs e)
+        {
+            Service service = new Service();
+            service.Location = textBoxAddress.Text;
+            service.Name = textBoxName.Text;
+            service.Description = richTextBoxDescription.Text;
+
+            _handler.AddService(textBoxAddress.Text,richTextBoxDescription.Text, )
         }
     }
 }
